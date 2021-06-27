7be6433bd8c617106fdd96f6e29ba00613eca1bc
HansRCode <12411150+HansRCode@users.noreply.github.com>
2021-06-27 13:53:35 -0400

    Generate product model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+	
 	validates :email, uniqueness: true 
 	validates_format_of :email, with: /@/ 
 	validates :password_digest, presence: true
 	has_secure_password
+
+	has_many :products, dependent: :destroy
+
 end
