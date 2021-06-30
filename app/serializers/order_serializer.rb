2fb9e61926e450be5b37c96b6c80f7b68ad77723
HansRCode <12411150+HansRCode@users.noreply.github.com>
2021-06-30 16:56:00 -0400

    Adds caching for the serializers

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -3,4 +3,7 @@ class OrderSerializer
   attributes 
   belongs_to :user
   has_many :products
+
+  cache_options enabled: true, cache_length: 12.hours
+  
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,7 @@ class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :email
   has_many :products
+
+  cache_options enabled: true, cache_length: 12.hours
+  
 end
